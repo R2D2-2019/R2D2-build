76132083201d5260f4d4131cc33696c10805634c
gerritvanos <31821855+gerritvanos@users.noreply.github.com>
2019-03-14 21:01:13 +0100

    added rpi compiler path

--- init.bat
+++ init.bat
@@ -5,7 +5,8 @@ set systemPath=%PATH%
 set bmptkPath=%repoPath%programs\bmptk\tools
 set cppPath=%repoPath%programs\mingw32\bin
 set armPath=%repoPath%programs\8 2018-q4-major\bin
+set rpiPath=%repoPath%programs\gcc_rpi3\armv8-rpi3-linux-gnueabihf\bin
 
-setx /m PATH "%systemPath%;%bmptkPath%;%cppPath%;%armPath%"
+setx /m PATH "%systemPath%;%bmptkPath%;%cppPath%;%armPath%;%rpiPath%"
 
 pause
